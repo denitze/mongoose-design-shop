3527e6cf100bd75f3123032d250ddced6e6c1a25
denitze <denise.brachwitz@gmail.com>
2021-01-18 15:27:26 +0100

    MVC done, not responsive

--- index.js
+++ index.js
@@ -8,7 +8,10 @@ app.set('view engine', 'ejs')
 app.use(express.json())
 app.use(express.urlencoded({ extended: true }))
 const mongoose = require('mongoose');
+const addRoutes = require('./routes/addRoutes')
+const singleRoutes = require('./routes/singleRoutes')
 const GalleryItem = require('./models/galleryItem')
+
 mongoose.connect(process.env.dbUri, { useNewUrlParser: true, useUnifiedTopology: true })
     .then(result => {
         console.log('Connected to my DB')
@@ -24,12 +27,12 @@ app.get('/', (req, res) => {
     .then(result => {
         // res.send(result)
         res.render('index', {galleryData: result})
-
     })
     .catch(err => console.log(err) )
 })
 
-// app.get('/add-new', (req, res) => {
+// Probedata
+//app.get('/add-new', (req, res) => {
 //     const newGalleryItem = new GalleryItem({
 //         url: 'https://images.unsplash.com/photo-1610368020268-2529f89bac93?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=1050&q=80',
 //         articlename: 'Vladyslav Tobolenko',
@@ -44,51 +47,5 @@ app.get('/', (req, res) => {
 //     .catch(err => console.log(err))
 // })
 
-
-app.get('/add', (req, res) => {
-    GalleryItem.find()
-    .then(result => {
-        const productData = result
-        const randomData = productData.sort(() => .5 - Math.random()).slice(0,6)
-        res.render('add', {productData, randomData})
-    })
-    .catch(err=> console.log(err))
-})
-
-app.post('/add-product', (req, res) => {
-    console.log(req.body);
-    const newGalleryItem = new GalleryItem({
-        url: req.body.url,
-        articlename: req.body.articlename,
-        price: req.body.price,
-        brandname: req.body.brandname,
-        description: req.body.description
-    })
-    newGalleryItem.save()
-    .then(result => {
-        res.redirect('/')
-    })
-    .catch(err => console.log(err))
-})
-
-app.get('/single/:pictureId', (req, res) => {
-    console.log(req.params.pictureId);
-    GalleryItem.findById(req.params.pictureId)
-    .then(result => {
-        res.render('single', {picture: result})
-    })
-    .catch(err => console.log(err))
-    })
-
-    app.get('/single/:pictureId/delete', (req, res) => {
-    GalleryItem.findByIdAndDelete(req.params.pictureId)
-    .then(result => res.redirect('/'))
-    .catch(err => console.log(err))
-})
-
-app.post('/single/:pictureId/edit', (req, res) => {
-    console.log(req.body)
-    GalleryItem.findByIdAndUpdate(req.params.pictureId, req.body)
-    .then(result => res.redirect(`/single/${req.params.pictureId}`))
-    .catch(err => console.log(err))
-})
+app.use('/add', addRoutes)
+app.use('/single', singleRoutes)
